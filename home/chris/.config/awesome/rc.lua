222e99fb1372fa21a4511563e5d4237fce3be735
Christoph Mertz <cmertz@users.noreply.github.com>
2019-08-27 14:18:06 +0200

    awesome: make startup position for new clients less confusing

--- home/chris/.config/awesome/rc.lua
+++ home/chris/.config/awesome/rc.lua
@@ -342,12 +342,13 @@
   -- {{{ Signals
   -- Signal function to execute when a new client appears.
   client.connect_signal('manage', function (c)
-      if awesome.startup and
-        not c.size_hints.user_position
-        and not c.size_hints.program_position then
-          -- Prevent clients from being unreachable after screen count changes.
-          awful.placement.no_offscreen(c)
-      end
+    if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
+        -- Prevent clients from being unreachable after screen count changes.
+        awful.placement.no_offscreen(c)
+    end
+    if not awesome.startup then
+      awful.client.setslave(c)
+    end
   end)
 
   client.connect_signal('mouse::enter', function(c) client.focus = c end)
