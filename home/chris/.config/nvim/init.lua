ef35bd08c5ae650ba72f570a4595d7a8028a2cf1
Christoph Mertz <cmertz@users.noreply.github.com>
2024-01-10 11:01:23 +0100

    nvim: rework signs

--- home/chris/.config/nvim/init.lua
+++ home/chris/.config/nvim/init.lua
@@ -16,10 +16,35 @@ local opt = vim.opt
 opt.signcolumn = 'yes'
 vim.cmd[[highlight SignColumn guibg=NONE]]
 
--- suppress text for errors / warnings
 vim.diagnostic.config({
+  -- suppress text for errors / warnings
   virtual_text =  false,
+
+  -- TODO nvim >= v0.10
+  -- signs = {
+  --   text = {
+  --     [vim.diagnostic.severity.ERROR] = '✘',
+  --     [vim.diagnostic.severity.WARN] = '▲',
+  --     [vim.diagnostic.severity.HINT] = '⚑',
+  --     [vim.diagnostic.severity.INFO] = '»',
+  --   },
+  -- },
 })
+
+-- TODO remove for nvim >= v0.10
+local function sign_define(args)
+  vim.fn.sign_define(args.name, {
+    texthl = args.name,
+    text = args.text,
+    numhl = ''
+  })
+end
+
+-- TODO remove for nvim >= v0.10
+sign_define({name = 'DiagnosticSignError', text = '✘'})
+sign_define({name = 'DiagnosticSignWarn',  text = '▲'})
+sign_define({name = 'DiagnosticSignHint',  text = '⚑'})
+sign_define({name = 'DiagnosticSignInfo',  text = '»'})
 
 opt.ttyfast = true
 
