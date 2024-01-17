cb1983c254ba053ffca8d07defc71a3634c2cefb
Christoph Mertz <cmertz@users.noreply.github.com>
2024-01-17 10:54:31 +0100

    nvim: use `hadolint` for `ft=dockerfile`

--- home/chris/.config/nvim/lua/format_lint.lua
+++ home/chris/.config/nvim/lua/format_lint.lua
@@ -1,8 +1,8 @@
 local ft = require('guard.filetype')
 
 ft('go'):fmt('lsp'):lint('golangci_lint')
-
 ft('sh'):fmt('shfmt'):lint('shellcheck')
+ft('dockerfile'):lint('hadolint')
 
 require('guard').setup({
 	fmt_on_save = true,
