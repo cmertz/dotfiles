745ab92a1407fafe2bcb2e89d41bb7b7cc794e6b
Christoph Mertz <cmertz@users.noreply.github.com>
2025-02-21 13:08:10 +0100

    nvim: update config

--- home/chris/.config/nvim/lua/format_lint.lua
+++ home/chris/.config/nvim/lua/format_lint.lua
@@ -1,18 +1,21 @@
 local ft = require('guard.filetype')
 
-ft('go'):fmt('gofumpt'):append('golines'):lint('golangci_lint')
+ft('go'):fmt({
+	cmd = 'goimports',
+	stdin = true
+}):append('gofumpt')
 ft('sh'):fmt('shfmt'):lint('shellcheck')
 ft('dockerfile'):lint('hadolint')
 ft('python'):fmt('ruff'):lint('ruff')
 
 vim.g.guard_config = {
-    fmt_on_save = true,
-    lsp_as_default_formatter = true,
-    save_on_fmt = true,
+	fmt_on_save = true,
+	save_on_fmt = true,
+	lsp_as_default_formatter = true,
 }
 
 vim.api.nvim_create_autocmd({ "BufWrite" }, {
-  pattern = { "go" , "sh", "dockerfile", "python" },
+  pattern = { "go", "sh", "dockerfile", "python" },
   callback = function(args)
     require('guard.format').do_fmt(args.buf)
   end
